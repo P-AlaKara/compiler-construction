18cd2097c4dad903a8f5f9bd409dac68604d330c
Priscilla Alakara <140266837+P-AlaKara@users.noreply.github.com>
2025-05-16 14:27:55 +0300

    Update semantic.c

--- semantic.c
+++ semantic.c
@@ -5,6 +5,8 @@
 
 typedef struct Symbol {
     char* name;
+    Type type;
+    int scope_level;
     struct Symbol* next;
 } Symbol;
 
@@ -19,9 +21,11 @@ int is_declared(const char* name) {
     return 0;
 }
 
-void declare(const char* name) {
+void declare(const char* name, Type type, int scope) {
     Symbol* sym = malloc(sizeof(Symbol));
     sym->name = strdup(name);
+    sym->type = type;
+    sym->scope_level = scope;
     sym->next = symbol_table;
     symbol_table = sym;
 }
@@ -42,14 +46,18 @@ void check_node(ASTNode* node) {
             break;
 
         case NODE_DECL:
-            if (is_declared(node->decl.var_name)) {
-                semantic_error("Variable redeclared", node->decl.var_name);
-            }
-            declare(node->decl.var_name);
-            if (node->decl.init_value) {
-                get_type(node->decl.init_value);
-            }
-            break;
+	  if (is_declared(node->decl.var_name)) {
+	    semantic_error("Variable redeclared", node->decl.var_name);
+	  }
+	  declare(node->decl.var_name, node->decl.declared_type, 0);
+	  if (node->decl.init_value) {
+	    Type init_type = get_type(node->decl.init_value);
+	    if (init_type != node->decl.declared_type) {
+	      semantic_error("Type mismatch in initialization", node->decl.var_name);
+	    }
+	  }
+	  break;
+
 
         case NODE_ASSIGN:
             if (!is_declared(node->assign.var_name)) {
@@ -88,42 +96,62 @@ void check_node(ASTNode* node) {
             break;
     }
 }
+
 Type get_type(ASTNode* node) {
     if (!node) return TYPE_ERROR;
 
     switch (node->type) {
         case NODE_INT:
             return TYPE_INT;
+	    
+    case NODE_BOOL:
+      return TYPE_BOOL;
 
-        case NODE_VAR:
-            if (!is_declared(node->var_name)) {
-                semantic_error("Use of undeclared variable", node->var_name);
-                return TYPE_ERROR;
-            }
-            return TYPE_INT;  // only int for now
+        case NODE_VAR: {
+    if (!is_declared(node->var_name)) {
+        semantic_error("Use of undeclared variable", node->var_name);
+    }
+    Symbol* curr = symbol_table;
+    while (curr) {
+        if (strcmp(curr->name, node->var_name) == 0) {
+            return curr->type;
+        }
+        curr = curr->next;
+    }
+    return TYPE_ERROR; // shouldn't happen
+}
+  
 
         case NODE_BINOP: {
             Type left = get_type(node->binop.left);
             Type right = get_type(node->binop.right);
             if (left != TYPE_INT || right != TYPE_INT) {
                 fprintf(stderr, "Type error: binary operator applied to non-int\n");
-                return TYPE_ERROR;
+                exit(1);
             }
             return TYPE_INT;
         }
 
-        case NODE_ASSIGN: {
-            Type rhs = get_type(node->assign.expr);
-            if (rhs != TYPE_INT) {
-                semantic_error("Type error: assigning non-int to int variable", node->assign.var_name);
+       case NODE_ASSIGN: {
+    Type rhs = get_type(node->assign.expr);
+
+    Symbol* curr = symbol_table;
+    while (curr) {
+        if (strcmp(curr->name, node->assign.var_name) == 0) {
+            if (curr->type != rhs) {
+                semantic_error("Type mismatch in assignment to variable", curr->name);
             }
-            return TYPE_INT;
+            return curr->type;
         }
+        curr = curr->next;
+    }
+
+    semantic_error("Assignment to undeclared variable", node->assign.var_name);
+    return TYPE_ERROR;
+}
 
         case NODE_DECL:
-            if (node->decl.init_value)
-                get_type(node->decl.init_value);
-            return TYPE_INT;
+            return node->decl.declared_type;
 
         case NODE_PRINT:
             get_type(node->print_expr);
@@ -145,3 +173,19 @@ Type get_type(ASTNode* node) {
 void semantic_check(ASTNode* root) {
     check_node(root);
 }
+
+void print_symbol_table() {
+    printf("Symbol Table:\n");
+    printf("%-10s | %-5s | %-5s\n", "Name", "Type", "Scope");
+    printf("-----------------------------\n");
+    Symbol* curr = symbol_table;
+    while (curr) {
+        const char* type_str = (curr->type == TYPE_INT) ? "int" :
+                       (curr->type == TYPE_BOOL) ? "bool" : "unknown";
+
+        printf("%-10s | %-5s | %-5s\n", curr->name, type_str, "global"); // only one scope for now
+        curr = curr->next;
+    }
+}
+
+
