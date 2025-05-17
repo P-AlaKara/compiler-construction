81fb2462276030012c8f67671ad628c813422977
Priscilla Alakara <140266837+P-AlaKara@users.noreply.github.com>
2025-05-17 16:15:02 +0300

    Fix bool error

--- semantic.c
+++ semantic.c
@@ -22,12 +22,18 @@ int is_declared(const char* name) {
 }
 
 void declare(const char* name, Type type, int scope) {
+    printf("DEBUG: Declaring symbol '%s' with type %d\n", name, type);
     Symbol* sym = malloc(sizeof(Symbol));
+    if (!sym) {
+        fprintf(stderr, "Memory allocation failed for symbol\n");
+        exit(1);
+    }
     sym->name = strdup(name);
     sym->type = type;
     sym->scope_level = scope;
     sym->next = symbol_table;
     symbol_table = sym;
+    printf("DEBUG: Symbol declared successfully\n");
 }
 
 void semantic_error(const char* msg, const char* name) {
@@ -35,94 +41,41 @@ void semantic_error(const char* msg, const char* name) {
     exit(1);
 }
 
-void check_node(ASTNode* node) {
-    if (!node) return;
+Type get_type(ASTNode* node) {
+    if (!node) {
+        printf("DEBUG: get_type called with NULL node\n");
+        return TYPE_ERROR;
+    }
+
+    printf("DEBUG: get_type processing node of type %d\n", node->type);
 
     switch (node->type) {
-        case NODE_STMT_LIST:
-            for (int i = 0; i < node->stmt_list.count; i++) {
-                check_node(node->stmt_list.stmts[i]);
-            }
-            break;
+        case NODE_INT:
+            printf("DEBUG: get_type - found INT node with value %d\n", node->int_value);
+            return TYPE_INT;
+            
+        case NODE_BOOL:
+            printf("DEBUG: get_type - found BOOL node with value %d\n", node->int_value);
+            return TYPE_BOOL;
 
-        case NODE_DECL:
-	  if (is_declared(node->decl.var_name)) {
-	    semantic_error("Variable redeclared", node->decl.var_name);
-	  }
-	  declare(node->decl.var_name, node->decl.declared_type, 0);
-	  if (node->decl.init_value) {
-	    Type init_type = get_type(node->decl.init_value);
-	    if (init_type != node->decl.declared_type) {
-	      semantic_error("Type mismatch in initialization", node->decl.var_name);
-	    }
-	  }
-	  break;
-
-
-        case NODE_ASSIGN:
-            if (!is_declared(node->assign.var_name)) {
-                semantic_error("Assignment to undeclared variable", node->assign.var_name);
-            }
-            get_type(node->assign.expr);
-            break;
-
-        case NODE_PRINT:
-            check_node(node->print_expr);
-            break;
-
-        case NODE_BINOP:
-            check_node(node->binop.left);
-            check_node(node->binop.right);
-            break;
-
-        case NODE_IF:
-            check_node(node->if_stmt.condition);
-            check_node(node->if_stmt.if_body);
-            if (node->if_stmt.else_body)
-                check_node(node->if_stmt.else_body);
-            break;
-
-        case NODE_VAR:
+        case NODE_VAR: {
+            printf("DEBUG: get_type - checking variable '%s'\n", node->var_name);
             if (!is_declared(node->var_name)) {
                 semantic_error("Use of undeclared variable", node->var_name);
             }
-            break;
-
-        case NODE_INT:
-            break; // always valid
-
-        default:
-            fprintf(stderr, "Unknown node type %d\n", node->type);
-            break;
-    }
-}
-
-Type get_type(ASTNode* node) {
-    if (!node) return TYPE_ERROR;
-
-    switch (node->type) {
-        case NODE_INT:
-            return TYPE_INT;
-	    
-    case NODE_BOOL:
-      return TYPE_BOOL;
-
-        case NODE_VAR: {
-    if (!is_declared(node->var_name)) {
-        semantic_error("Use of undeclared variable", node->var_name);
-    }
-    Symbol* curr = symbol_table;
-    while (curr) {
-        if (strcmp(curr->name, node->var_name) == 0) {
-            return curr->type;
+            Symbol* curr = symbol_table;
+            while (curr) {
+                if (strcmp(curr->name, node->var_name) == 0) {
+                    printf("DEBUG: get_type - variable '%s' has type %d\n", node->var_name, curr->type);
+                    return curr->type;
+                }
+                curr = curr->next;
+            }
+            return TYPE_ERROR; // shouldn't happen
         }
-        curr = curr->next;
-    }
-    return TYPE_ERROR; // shouldn't happen
-}
-  
 
         case NODE_BINOP: {
+            printf("DEBUG: get_type - processing binary operation '%s'\n", node->binop.op);
             Type left = get_type(node->binop.left);
             Type right = get_type(node->binop.right);
             if (left != TYPE_INT || right != TYPE_INT) {
@@ -132,32 +85,37 @@ Type get_type(ASTNode* node) {
             return TYPE_INT;
         }
 
-       case NODE_ASSIGN: {
-    Type rhs = get_type(node->assign.expr);
+        case NODE_ASSIGN: {
+            printf("DEBUG: get_type - processing assignment to '%s'\n", node->assign.var_name);
+            Type rhs = get_type(node->assign.expr);
 
-    Symbol* curr = symbol_table;
-    while (curr) {
-        if (strcmp(curr->name, node->assign.var_name) == 0) {
-            if (curr->type != rhs) {
-                semantic_error("Type mismatch in assignment to variable", curr->name);
+            Symbol* curr = symbol_table;
+            while (curr) {
+                if (strcmp(curr->name, node->assign.var_name) == 0) {
+                    if (curr->type != rhs) {
+                        semantic_error("Type mismatch in assignment to variable", curr->name);
+                    }
+                    return curr->type;
+                }
+                curr = curr->next;
             }
-            return curr->type;
-        }
-        curr = curr->next;
-    }
 
-    semantic_error("Assignment to undeclared variable", node->assign.var_name);
-    return TYPE_ERROR;
-}
+            semantic_error("Assignment to undeclared variable", node->assign.var_name);
+            return TYPE_ERROR;
+        }
 
         case NODE_DECL:
+            printf("DEBUG: get_type - processing declaration of '%s' with type %d\n", 
+                  node->decl.var_name, node->decl.declared_type);
             return node->decl.declared_type;
 
         case NODE_PRINT:
+            printf("DEBUG: get_type - processing print statement\n");
             get_type(node->print_expr);
             return TYPE_INT;
 
         case NODE_IF:
+            printf("DEBUG: get_type - processing if statement\n");
             get_type(node->if_stmt.condition);
             get_type(node->if_stmt.if_body);
             if (node->if_stmt.else_body)
@@ -170,8 +128,111 @@ Type get_type(ASTNode* node) {
     }
 }
 
+void check_node(ASTNode* node) {
+    if (!node) {
+        printf("DEBUG: check_node called with NULL node\n");
+        return;
+    }
+
+    printf("DEBUG: check_node processing node of type %d\n", node->type);
+
+    switch (node->type) {
+        case NODE_STMT_LIST:
+            printf("DEBUG: check_node - processing statement list with %d statements\n", 
+                   node->stmt_list.count);
+            for (int i = 0; i < node->stmt_list.count; i++) {
+                check_node(node->stmt_list.stmts[i]);
+            }
+            break;
+
+        case NODE_DECL:
+            printf("DEBUG: check_node - processing declaration of '%s'\n", node->decl.var_name);
+            if (is_declared(node->decl.var_name)) {
+                semantic_error("Variable redeclared", node->decl.var_name);
+            }
+            declare(node->decl.var_name, node->decl.declared_type, 0);
+            if (node->decl.init_value) {
+                printf("DEBUG: About to get type of initializer for %s, node type: %d\n", 
+                       node->decl.var_name, node->decl.init_value->type);
+                printf("DEBUG: Initializer address: %p\n", (void*)node->decl.init_value);
+                Type init_type = get_type(node->decl.init_value);
+                printf("DEBUG: Got type %d for initializer\n", init_type);
+                if (init_type != node->decl.declared_type) {
+                    semantic_error("Type mismatch in initialization", node->decl.var_name);
+                }
+            }
+            break;
+
+        case NODE_ASSIGN:
+            printf("DEBUG: check_node - processing assignment to '%s'\n", node->assign.var_name);
+            if (!is_declared(node->assign.var_name)) {
+                semantic_error("Assignment to undeclared variable", node->assign.var_name);
+            }
+            get_type(node->assign.expr);
+            break;
+
+        case NODE_PRINT:
+            printf("DEBUG: check_node - processing print statement\n");
+            check_node(node->print_expr);
+            break;
+
+        case NODE_BINOP:
+            printf("DEBUG: check_node - processing binary operation\n");
+            check_node(node->binop.left);
+            check_node(node->binop.right);
+            break;
+
+        case NODE_IF:
+            printf("DEBUG: check_node - processing if statement\n");
+            check_node(node->if_stmt.condition);
+            check_node(node->if_stmt.if_body);
+            if (node->if_stmt.else_body)
+                check_node(node->if_stmt.else_body);
+            break;
+
+        case NODE_VAR:
+            printf("DEBUG: check_node - processing variable '%s'\n", node->var_name);
+            if (!is_declared(node->var_name)) {
+                semantic_error("Use of undeclared variable", node->var_name);
+            }
+            break;
+
+        case NODE_INT:
+            printf("DEBUG: check_node - processing integer value: %d\n", node->int_value);
+            break;
+            
+        case NODE_BOOL:
+            printf("DEBUG: check_node - processing boolean value: %d\n", node->int_value);
+            break;
+
+        default:
+            fprintf(stderr, "Unknown node type %d\n", node->type);
+            break;
+    }
+}
+
 void semantic_check(ASTNode* root) {
+    printf("Starting semantic check...\n");
+    if (!root) {
+        printf("ERROR: semantic_check received NULL root\n");
+        return;
+    }
+    printf("Root node type: %d\n", root->type);
+    
+    // If it's a statement list, let's print debug info about it
+    if (root->type == NODE_STMT_LIST) {
+        printf("Statement list has %d statements\n", root->stmt_list.count);
+        for (int i = 0; i < root->stmt_list.count; i++) {
+            if (root->stmt_list.stmts[i]) {
+                printf("Statement %d has type %d\n", i, root->stmt_list.stmts[i]->type);
+            } else {
+                printf("Statement %d is NULL\n", i);
+            }
+        }
+    }
+    
     check_node(root);
+    printf("Semantic check completed.\n");
 }
 
 void print_symbol_table() {
@@ -187,5 +248,3 @@ void print_symbol_table() {
         curr = curr->next;
     }
 }
-
-
